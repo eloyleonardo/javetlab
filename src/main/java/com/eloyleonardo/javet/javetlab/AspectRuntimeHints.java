b058ff03bfee99dbc1d8d48077b3b6008f96f1f7
Leonardo Eloy <leonardo@devena.com>
2024-04-30 08:37:47 -0300

    Added predefined classes

--- src/main/java/com/eloyleonardo/javet/javetlab/AspectRuntimeHints.java
+++ src/main/java/com/eloyleonardo/javet/javetlab/AspectRuntimeHints.java
@@ -1,8 +1,14 @@
 package com.eloyleonardo.javet.javetlab;
 
+import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+
+import com.google.common.reflect.ClassPath;
 
 public class AspectRuntimeHints implements RuntimeHintsRegistrar {
 
@@ -11,5 +17,20 @@ public class AspectRuntimeHints implements RuntimeHintsRegistrar {
         hints.reflection().registerType(com.caoccao.javet.interop.loader.JavetLibLoader.class, builder -> builder.withMembers(MemberCategory.values()));
         hints.reflection().registerType(com.caoccao.javet.interop.V8Host.class, builder -> builder.withMembers(MemberCategory.values()));
         hints.reflection().registerType(com.caoccao.javet.values.reference.IV8Module.class, builder -> builder.withMembers(MemberCategory.values()));
+
+        hints.reflection().registerTypeIfPresent(classLoader, "com.caoccao.javet.interop.loader.JavetLibLoader", MemberCategory.values());
+        hints.reflection().registerTypeIfPresent(classLoader, "com.caoccao.javet.interop.NodeNative", MemberCategory.values());
+        hints.reflection().registerTypeIfPresent(classLoader, "com.caoccao.javet.interop.V8Native", MemberCategory.values());
+
+        Set<Class<?>> allJavetClasses = null;
+        try {
+            allJavetClasses = ClassPath.from(classLoader).getAllClasses().stream().filter(clazz -> clazz.getPackageName().startsWith("com.caoccao")).map(clazz -> clazz.load()).filter(c -> c != null).collect(Collectors.toSet());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (Class<?> clazz : allJavetClasses) {
+            hints.reflection().registerType(clazz, builder -> builder.withMembers(MemberCategory.values()));
+        }
     }
 }
